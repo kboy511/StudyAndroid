75b7c2bf174c2a191c7f40c9528de4d99c0efa0b
kboy511 <kboy511@gmail.com>
2016-03-20 17:58:32 +0800

    初始化--简单聊天界面

---
+++ app/src/main/java/com/example/zengyuping/studyandroid/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.zengyuping.studyandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnMsgListView;
+    //private int temp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnMsgListView=(Button)findViewById(R.id.btnMsgListView);
+        btnMsgListView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_MsgListView =new Intent(MainActivity.this,MsgListView.class);
+                startActivity(intent_MsgListView);
+            }
+        });
+    }
+}
